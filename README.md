0aff91231beea8cfe636742d84da6bfa270195fa
SlimeyDev <96341847+SlimeyDev@users.noreply.github.com>
2023-05-05 11:20:14 +0530

    SpellGPT discontinued

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SpellGPT
 
 Welcome to my open-source GitHub repository for a web application that utilizes the GPT-3.5 API to correct spelling and grammar mistakes in written content. This application, developed by SlimeyDev, offers a powerful tool for anyone looking to improve their writing skills. With an intuitive and user-friendly interface, users can quickly input their text and receive accurate and contextually relevant suggestions for corrections. The open-source nature of the application allows developers to contribute to the project and improve its functionality. With high accuracy and a focus on simplicity, this web application is ideal for individuals, students, and writers looking to enhance the quality of their written work.
+
+# Note:
+
+This project has been taken down due to not being able to access the openai API as I have run out of the free credits for the API.
+Feel free to reuse this code.
--- app.py
+++ app.py
@@ -15,165 +15,37 @@ def process():
     text = request.form["text"]
     mode = request.form["modes"]
     #checking if text is entered or not
-    if text == "Paste your text here!" or text.isspace() or text == "":
+    if text.isspace() or text == "":
         return render_template("spellGPT.html", text="ERROR: No text provided")
+    key = os.getenv("OPEN_AI")
+    # processing the text
+    if mode == "SnG":
+        prompt = "correct the grammar and spelling in this text and only return the corrected text only:" + text
+    elif mode == "formal":
+        prompt = "change the tone of this text to formal without compromising its meaning and only return the new text only: " + text
+    elif mode == "informal":
+        prompt = "change the tone of this text to informal without compromising its meaning and only return the new text only: " + text
+    elif mode == "casual":
+        prompt = "change the tone of this text to casual without compromising its meaning and only return the new text only: " + text
+    elif mode == "shakespeare":
+        prompt = "Change the way this text has been written to the way shakespeare would have written it and only return the new text only: " + text
+    elif mode == "GenZ":
+        prompt = "Change the way this text has been written to the way a GenZ would have written it and only return the new text only: " + text
     else:
-        #outputting the processed text
-        key = os.getenv("OPEN_AI")
-        #Spelling and grammar mode
-        if mode == "SnG":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "correct the grammar and spelling in this text and only return the corrected text only:"+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("SnG")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #formal tone mode
-        elif mode == "formal":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "change the tone of this text to formal without compromising its meaning and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("formal")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #informal tone mode
-        elif mode == "informal":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "change the tone of this text to informal without compromising its meaning and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("informal")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #casual tone mode
-        elif mode == "casual":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "change the tone of this text to casual without compromising its meaning and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("casual")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #shakespeare mode
-        elif mode == "shakespeare":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "Change the way this text has been written to the way shakespeare would have written it and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("shakespeare")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #genz mode
-        elif mode == "GenZ":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "Change the way this text has been written to the way a GenZ would have written it and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("GenZ")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #optimistic tone mode
-        elif mode == "Optimistic":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "change the tone of this text to optimistic without compromising its meaning and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("Optimistic")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #worried tone mode
-        elif mode == "Worried":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "change the tone of this text to worried without compromising its meaning and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("Worried")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #friendly tone mode
-        elif mode == "Friendly":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "change the tone of this text to friendly without compromising its meaning and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("Friendly")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #cooperative tone mode
-        elif mode == "Cooperative":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "change the tone of this text to cooperative without compromising its meaning and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("Friendly")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
-        #surprised tone mode
-        elif mode == "Surprised":
-            openai.api_key = key
-            messages = []
-            system_msg = "chat"
-            messages.append({"role": "system", "content": system_msg})
-            messages.append({"role": "user", "content": "change the tone of this text to surprised without compromising its meaning and only return the new text only: "+text})
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-            reply = response["choices"][0]["message"]["content"]
-            messages.append({"role": "assistant", "content": reply})
-            print("Friendly")
-            print(reply)
-            processed_text = reply
-            return render_template("spellGPT.html", text=processed_text, mode=mode)
+        processed_text = "An unknown error occurred!"
+        return render_template("spellGPT.html", text=processed_text, mode=mode)
+
+    openai.api_key = key
+    messages = []
+    system_msg = "chat"
+    messages.append({"role": "system", "content": system_msg})
+    messages.append({"role": "user", "content": prompt})
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
+    processed_text = reply
+    
+    return render_template("spellGPT.html", text=processed_text, mode=mode)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
